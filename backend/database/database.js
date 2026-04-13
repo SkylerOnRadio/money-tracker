08ad48d9c3346ca4b0d5feeb1e786ea65c4638b3
SkylerOnAir <abhigyadulal@gmail.com>
2026-04-13 11:49:06 +0530

    Updated database connection to create transactions table if it is not present

--- backend/database/database.js
+++ backend/database/database.js
@@ -8,6 +8,9 @@ const db = new sqlite.Database("./database.sqlite", (err) => {
 		db.run(
 			"CREATE TABLE IF NOT EXISTS users (name TEXT PRIMARY KEY, pfpUrl TEXT)",
 		);
+		db.run(
+			"CREATE TABLE IF NOT EXISTS transactions (tid INTEGER PRIMARY KEY, date TEXT, time TEXT, t_to TEXT, t_from TEXT, amount REAL, type TEXT)",
+		);
 	}
 });
 
